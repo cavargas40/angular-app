3914161248faddc8ba02539b495f709362ceb7d3
Carlos Andres Vargas Lopez <cavargas40@gmail.com>
2020-02-05 20:34:24 -0500

    ngIf and ngElse, ngStyle, ngClass

--- src/app/components/server/server.component.ts
+++ src/app/components/server/server.component.ts
@@ -10,13 +10,19 @@ export class ServerComponent implements OnInit {
   public serverId: number = 10;
   public serverStatus: string = 'offline';
 
-  constructor() { }
+  constructor() {
+    this.serverStatus = Math.random() > 0.5 ? 'online': 'offline';
+  }
 
   ngOnInit() {
   }
 
-  get getServerStatus(){
+  get getServerStatus() {
     return this.serverStatus;
+  }
+
+  get getColor(){
+    return this.serverStatus === 'online' ? 'green' : 'red';
   }
 
 }
